e7ef5e8b976753a5326d6e10953555f054244fe8
slopez34 <slopez34@cuc.edu.co>
2024-09-29 23:01:20 -0500

    primera version

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import milogo from "../src/assets/img/Imagen_Betplay.png"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={milogo} alt="Logo de BetPlay"/>
+        <h1>BetPlay</h1>
+        <p>BetPlay es una plataforma de apuestas en linea de deportes y casinos en vivo.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
